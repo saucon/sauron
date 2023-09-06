f1a29d2364b3d2fad22651acc0b518c02381d261
saucon <saucon.mfh@gmail.com>
2023-09-07 04:53:03 +0800

    update mod

--- v2/main.go
+++ v2/main.go
@@ -12,11 +12,6 @@ func main() {
 	logger := log.NewLogCustom(&logconfig.Config{}, false)
 	logger.PrettyPrintJSON(true)
 
-	//logger.Fatal(log2.LogData{
-	//	Description: "main fatal",
-	//	StartTime:   timeStart,
-	//})
-
 	logger.Success(log.LogData{
 		Description: "main success",
 		StartTime:   timeStart,
